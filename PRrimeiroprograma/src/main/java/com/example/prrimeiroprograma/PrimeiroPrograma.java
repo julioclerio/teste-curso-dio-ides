4856ed91be4b9594b11bfb681e173d89691bb1b5
julioclerio <fernandes.clerio@gmail.com>
2022-10-05 12:54:40 -0300

    first commit aula dio

---
+++ PRrimeiroprograma/src/main/java/com/example/prrimeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.example.prrimeiroprograma;
+
+import com.example.prrimeiroprograma.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+        System.out.println(gato);
+
+
+    }
+}
